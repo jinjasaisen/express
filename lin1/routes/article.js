41cd266b2e786e0faf1a92a9c9d721aa7a9d5f66
jinjasaisen <jinjasaisen@gmail.com>
2015-08-18 19:59:05 +0900

    add new

--- lin1/routes/article.js
+++ lin1/routes/article.js
@@ -8,24 +8,21 @@ router.get('/article', function(req, res) {
 
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('data/linux.db');
-var check;
 
-db.each('SELECT title FROM linux where term=1', function(err, row) {
-    if(err !== null) {
-      next(err);
-    }
-    else {
-      console.log(row);
-      res.render('article', {title: row.title, pagename: 'article Post' },function(err, html) {
-        res.send(200, html);
-      });
-    }
+var posts=[];
+
+db.serialize(function() {
+    db.each("SELECT * FROM linux where term=1", function(err, row) {
+        posts.push({title: row.title, term: row.term })
+    }, function() {
+        // All done fetching records, render response
+        res.render("article", {pagename: "sqlite", posts: posts})
+    })
+});
+
+
   });
 
-
-//res.render('article', { pagename: 'article Post' });
-    
-});
 
 router.post('/art_post', function(req, res) {
 
